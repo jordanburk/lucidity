3411e5d09e8c302a1080d86c7262cf3cda9e3a5e
Jordan Burk <jordan.g.burk@gmail.com>
2017-06-08 22:15:47 -0700

    browserify test

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-var page = require('node_modules/page/index.js')
+var page = require('page')
 
 page('/', root)
-page('/user/:id', user.load, user.show)
-page('/user/:id/edit', user.load, user.edit)
-page('*', notfound)
+// page('/user/:id', user.load, user.show)
+// page('/user/:id/edit', user.load, user.edit)
+// page('*', notfound)
 
 function root(...args) {
   console.log(args)
