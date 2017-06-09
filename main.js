2aa4748ff6416f43a309c64b5311a356cda463f6
Jordan Burk <jordan.g.burk@gmail.com>
2017-06-08 22:32:58 -0700

    more testing

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 var page = require('page');
 
+console.log(page);
+
 page('/', logRootRoute);
 page('/:argument', argument);
 // page('/user/:id', user.load, user.show)
 // page('/user/:id/edit', user.load, user.edit)
 page('*', somethingElse);
+page();
 
 function logRootRoute(...args) {
   console.log('root');
