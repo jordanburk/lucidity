56034bd6e16c0426f29c4f11d7de072d5a9374a0
Jordan Burk <jordan.g.burk@gmail.com>
2017-06-08 22:30:45 -0700

    more testing

--- bundle.js
+++ bundle.js
@@ -1,24 +1,24 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-require('page')
+var page = require('page');
 
-page('/', logRootRoute)
-page('/:argument', argument)
+page('/', logRootRoute);
+page('/:argument', argument);
 // page('/user/:id', user.load, user.show)
 // page('/user/:id/edit', user.load, user.edit)
-page('*', somethingElse)
+page('*', somethingElse);
 
 function logRootRoute(...args) {
-  console.log('root')
-  console.log(args)
+  console.log('root');
+  console.log(args);
 }
 
 function argument(...args) {
-  console.log(args)
+  console.log(args);
 }
 
 function somethingElse(...args) {
-  console.log('other!')
-  console.log(args)
+  console.log('other!');
+  console.log(args);
 }
 
 },{"page":3}],2:[function(require,module,exports){
--- main.js
+++ main.js
@@ -1,21 +1,21 @@
-require('page')
+var page = require('page');
 
-page('/', logRootRoute)
-page('/:argument', argument)
+page('/', logRootRoute);
+page('/:argument', argument);
 // page('/user/:id', user.load, user.show)
 // page('/user/:id/edit', user.load, user.edit)
-page('*', somethingElse)
+page('*', somethingElse);
 
 function logRootRoute(...args) {
-  console.log('root')
-  console.log(args)
+  console.log('root');
+  console.log(args);
 }
 
 function argument(...args) {
-  console.log(args)
+  console.log(args);
 }
 
 function somethingElse(...args) {
-  console.log('other!')
-  console.log(args)
+  console.log('other!');
+  console.log(args);
 }
